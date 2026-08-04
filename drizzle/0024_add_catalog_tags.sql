6120ffda1d33ac11dc0363c74986dee4b611a186
AbdulrahmanNahhas <nahhas909@protonmail.com>
2026-08-04 15:00:20 +0300

    feat: add catalog taxonomy tags

---
+++ drizzle/0024_add_catalog_tags.sql
@@ -0,0 +1,8 @@
+INSERT OR IGNORE INTO terms (id, vocabulary, name, slug, label_ar, description, description_ar) VALUES
+  ('4cf0532a292ac365a91b3e5712aed93a', 'tag', 'Afterlife', 'afterlife', 'الحياة الآخرة', '', ''),
+  ('3478dbe6fde16a0fc4263465b23f71ec', 'tag', 'Satire', 'satire', 'السخرية', '', ''),
+  ('cd70371b98f499244913bbd71d1df706', 'tag', 'Shapeshifting', 'shapeshifting', 'تغيير الشكل', '', ''),
+  ('dc20efcb93e553a73d044a6f005d7376', 'tag', 'Superheroes', 'superheroes', 'الأبطال الخارقون', '', ''),
+  ('ee3a346f513be04972ce53be9f1c1bf7', 'tag', 'Surveillance', 'surveillance', 'المراقبة', '', ''),
+  ('6d4b781eef71fa4acb7997046b3f24ef', 'tag', 'Time Skip', 'time-skip', 'القفزة الزمنية', '', ''),
+  ('02c6dd5a8503032a69c13821a9bf50b8', 'tag', 'Trauma', 'trauma', 'الصدمة النفسية', '', '');
